7acf023a8b2aedfca2a1f15a049ad5cbb6bc0c30
anelson0828 <anelson0828@users.noreply.github.com>
2018-05-20 03:47:52 +0000

    20180520T034734Z

---
+++ cash.c
@@ -0,0 +1,60 @@
+// cash
+
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+//first prompt the user for a valid dollar amount they need change for (greater than 0)
+
+int main(void)
+{
+    float dollars;
+    do
+    {
+        printf("Change: \n");
+        dollars = get_float();
+    }
+    while (dollars < 0);
+
+//convert dollars into cents with the round function and figure out how many coins are needed to give change
+
+    int cents = round(dollars * 100);
+    int coins = 0;
+
+//start with 0 coins and add a coin for each amount of cents subtracted until there is no change remaining. Start by seeing if a Quarter can be used to make change
+
+    while (cents >= 25)
+
+    {
+        cents -= 25;
+        coins ++;
+    }
+
+//then see if a dime can be used
+
+    while (cents >= 10)
+    {
+        cents -= 10;
+        coins ++;
+    }
+
+//then see if a nickel can be used
+
+    while (cents >= 5)
+    {
+        cents -= 5;
+        coins ++;
+    }
+
+//and finally see if pennies need to be used to give the remaining change
+
+    while (cents >= 1)
+    {
+        cents -= 1;
+        coins ++;
+    }
+
+//print how many conis were needed to give correct change
+
+    printf("%i\n", coins);
+}
